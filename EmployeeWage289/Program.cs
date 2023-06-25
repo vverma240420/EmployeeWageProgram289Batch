e4b1dbde360042231019dc6d27e01e8399fd52fd
Vivek Verma <vverma240420@gmail.com>
2023-06-25 12:24:05 +0530

    Welcome to Employee Wage Computation Program

---
+++ EmployeeWage289/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeWage289
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+        }
+    }
+}
