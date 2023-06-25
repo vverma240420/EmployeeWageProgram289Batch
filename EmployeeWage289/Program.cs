14ee8a12083f5735516d2e3927447688a345c054
Vivek Verma <vverma240420@gmail.com>
2023-06-25 12:41:28 +0530

    Added Employee Attendance

--- EmployeeWage289/Program.cs
+++ EmployeeWage289/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            EmployeeAttendance.EmpAttendance();
         }
     }
 }
