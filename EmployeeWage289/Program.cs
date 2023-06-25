b663a43ea6bd851e60680e6ecd704b0e3daab4e2
Vivek Verma <vverma240420@gmail.com>
2023-06-25 14:10:33 +0530

    Added Switch Case statement

--- EmployeeWage289/Program.cs
+++ EmployeeWage289/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            PartTimeWage.PartTime();
+            UsingSwitchCase.SwitchCase();
         }
     }
 }
---
+++ EmployeeWage289/UsingSwitchCase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage289
+{
+    internal class UsingSwitchCase
+    {
+        public static void SwitchCase()
+        {
+            const int FULL_TIME = 1;
+            const int PART_TIME = 2;
+            const int EMP_RATE_PER_HR = 20;
+            int empHrs = 0, empWage = 0;
+
+            Console.WriteLine("Welcome to Employee Wage Computation");
+
+            //UC4- SwitchCase
+            Random random = new Random();
+            int employeeAttendance = random.Next(0, 3); // 0 or 1 or 2
+            switch (employeeAttendance) 
+            {
+                case FULL_TIME:
+                    Console.WriteLine("Full time employee is present");
+                    empHrs = 8;
+                    break;
+                case PART_TIME:
+                    Console.WriteLine("Part time employee is present");
+                    empHrs = 4;
+                    break;
+                  
+                default:
+                    Console.WriteLine("Employee is absent");
+                    break;
+            }
+            empWage = empHrs * EMP_RATE_PER_HR;
+            //Console.WriteLine("Employee wage:"+empWage);  old way
+            Console.WriteLine("Employee Wage:{0}", empWage);  //new way
+            Console.ReadLine();
+        }
+    }
+}
